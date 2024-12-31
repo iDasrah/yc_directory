bbdcd59050f09ca39b54a27d2e768461d4ce047d
dasrah <dasrah.dev@gmail.com>
2024-12-31 01:25:14 +0100

    fonction recherche

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,6 +1,7 @@
 import {defineQuery} from "groq";
 
-export const STARTUP_QUERY= defineQuery(`*[_type == "startup" && defined(slug.current)] | order(_createdAt, desc) {
+export const STARTUP_QUERY= defineQuery(`*[_type == "startup" && defined(slug.current)
+&& !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt, desc) {
   _id,
   _createdAt,
   pitch,
